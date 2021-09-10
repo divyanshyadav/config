1fa9b4ebf9bd7d4c039dacb9d38efe0c208828ae
Divyansh <divyansh.yadav@gmail.com>
2021-09-10 17:49:25 +0530

    update config file

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,16 @@ const isProd = String(process.env.NODE_ENV) === 'production'
 const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
-    // helps webpack in tree-shaking(if we disable modules)
-    presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }]],
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                // helps webpack in tree-shaking(if we disable modules)
+                modules: isTest ? 'commonjs' : false,
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+    ],
 }
